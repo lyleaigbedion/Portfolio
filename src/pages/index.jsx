6d8cdfca8c5ea5f9ee815462bb99ad370ffeb75a
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-08-20 12:45:25 -0400

    added rounded edges.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ export default () => (
         <>
           <AboutMe />
           {/* <Skills /> */}
+          <div style={{ paddingBottom: '15%' }} />
         </>
       </SidebarWrapper>
     </Layout>
